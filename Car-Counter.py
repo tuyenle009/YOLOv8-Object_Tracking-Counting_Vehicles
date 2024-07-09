2e909139f125b3d606726e179e24f59aa764cc24
tuyenle009 <leductuyen099@gmail.com>
2024-07-09 20:52:27 +0700

    update

---
+++ Car-Counter.py
@@ -0,0 +1,113 @@
+import random
+from ultralytics import YOLO
+import cv2
+import cvzone
+from tracker import *
+
+# Initialize the webcam (commented out)
+# cap = cv2.VideoCapture(0)
+# cap.set(3,1280) # Set the width
+# cap.set(4,720) # Set the height
+
+# Initialize video capture from a file
+cap = cv2.VideoCapture("Images_Videos/vehicles.mp4")
+
+# Load the YOLO model with specific weights
+model = YOLO("../Yolo-Weights/yolov8n.pt")
+
+# List of class names that the YOLO model can detect
+classNames = [
+    "person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+    "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+    "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+    "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+    "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+    "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+    "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+    "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+    "teddy bear", "hair drier", "toothbrush"
+]
+
+# Load the mask image for the region of interest
+mask = cv2.imread("Images_Videos/mask.png")
+
+# Load the graphics image to overlay on the video frame
+imgGraphics = cv2.imread("Images_Videos/carGraphics.png", cv2.IMREAD_UNCHANGED)
+
+# Initialize the tracker for object tracking
+tracker = EuclideanDistTracker()
+
+# Define the limits for counting objects crossing a line
+limits = [20, 275, 230, 275]
+
+# List to keep track of total counted objects
+totalCount = []
+colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for i in range(10)]
+while True:
+    # Read a frame from the video
+    flag, frame = cap.read()
+
+    # Apply the mask to the frame to focus on the region of interest
+    frameRegion = cv2.bitwise_and(frame, mask)
+
+    # Draw a rectangle for displaying the count on the frame
+    cv2.rectangle(frame, (0, 0), (180, 50), (255, 255, 255), cv2.FILLED)
+
+    # Overlay the graphics image on the frame
+    frame = cvzone.overlayPNG(frame, imgGraphics, (0, 10))
+
+    # Use the YOLO model to detect objects in the region of interest
+    results = model(frameRegion, stream=True)
+
+    # Draw a line on the frame for counting objects
+    cv2.line(frame, (limits[0], limits[1]), (limits[2], limits[3]), (15, 60, 255), 5)
+
+    # Process each detection result
+    for r in results:
+        # List to store detections
+        detections = []
+        obj = r.boxes
+
+        # Extract class, confidence, and bounding box information for each detected object
+        for cls, conf, bbox in zip(obj.cls, obj.conf, obj.xywh):
+            x, y, w, h = bbox
+            x, y, w, h, cls = int(x), int(y), int(w), int(h), int(cls)
+
+            # Round confidence and get the class name
+            conf = math.ceil((conf * 100)) / 100
+            currentClass = classNames[cls]
+
+            # Draw bounding boxes for specific classes with confidence greater than 0.3
+            if currentClass == "car" or currentClass == "truck" or currentClass == "bus" and conf > 0.3:
+                detections.append([x, y, w, h])
+
+        # Perform object tracking
+        boxes_ids = tracker.update(detections)
+
+        # Annotate the frame with tracking IDs
+        for box_id in boxes_ids:
+            x, y, w, h, id = box_id
+            x1, y1 = int(x - w / 2), int(y - h / 2)
+
+            frame = tracker.opacityRectangle(frame, bbox=(x, y, w, h), color=colors[id % len(colors)])
+            cvzone.putTextRect(frame, f"{id}", (x, y), scale=1, thickness=2, offset=1)
+
+            # Check if the object crosses the defined line and update the count
+            if limits[0] < x < limits[2] and limits[3] - 10 < y < limits[3] + 5:
+                if totalCount.count(id) == 0:
+                    totalCount.append(id)
+
+    # Display the total count on the frame
+    cv2.putText(frame, str(len(totalCount)), (105, 38), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 3)
+
+    # Show the frame in a window
+    cv2.imshow("Image", frame)
+
+    # Break the loop if the 'q' key is pressed
+    if cv2.waitKey(9) == ord('q'):
+        break
+
+# Release the video capture object and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
---
+++ tracker.py
@@ -0,0 +1,71 @@
+import math
+import cv2
+import numpy as np
+
+
+class EuclideanDistTracker:
+    def __init__(self):
+        # Store the center positions of the objects
+        self.center_points = {}
+        # Keep the count of the IDs
+        # each time a new object id detected, the count will increase by one
+        self.id_count = 0
+
+    def update(self, objects_rect):
+        # Objects boxes and ids
+        objects_bbs_ids = []
+
+        # Get center point of new object
+        for rect in objects_rect:
+            x, y, w, h = rect
+            cx = (x + x + w) // 2
+            cy = (y + y + h) // 2
+
+            # Find out if that object was detected already
+            same_object_detected = False
+            for id, pt in self.center_points.items():
+                dist = math.hypot(cx - pt[0], cy - pt[1])
+
+                if dist < 25:
+                    self.center_points[id] = (cx, cy)
+                    # print(self.center_points)
+                    objects_bbs_ids.append([x, y, w, h, id])
+                    same_object_detected = True
+                    break
+
+            # New object is detected we assign the ID to that object
+            if same_object_detected is False:
+                self.center_points[self.id_count] = (cx, cy)
+                objects_bbs_ids.append([x, y, w, h, self.id_count])
+                self.id_count += 1
+
+        # Clean the dictionary by center points to remove IDS not used anymore
+        new_center_points = {}
+        for obj_bb_id in objects_bbs_ids:
+            _, _, _, _, object_id = obj_bb_id
+            center = self.center_points[object_id]
+            new_center_points[object_id] = center
+
+        # Update dictionary with IDs not used removed
+        self.center_points = new_center_points.copy()
+        return objects_bbs_ids
+
+    #draw opacity rectangle
+    def opacityRectangle(self, img, bbox, color):
+        x, y, w, h = bbox
+        x1, y1 = int(x - w / 2), int(y - h / 2)
+        x2, y2 = x1 + w, y1 + h
+
+        # Initialize blank mask image of same dimensions for drawing the shapes
+        shapes = np.zeros_like(img, np.uint8)
+
+        # Draw shapes
+        cv2.rectangle(shapes, (x1, y1), (x2, y2), color, cv2.FILLED)
+
+        # Generate output by blending image with shapes image, using the shapes
+        # images also as mask to limit the blending to those parts
+        out = img.copy()
+        alpha = 0.5
+        mask = shapes.astype(bool)
+        out[mask] = cv2.addWeighted(img, alpha, shapes, 1 - alpha, 0)[mask]
+        return out
